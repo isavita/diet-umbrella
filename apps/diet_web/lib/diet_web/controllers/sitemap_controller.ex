150c9a8e836cd811cb8cfd31264af5fca7229afd
Aleksandar Dimov <isavitaisa@gmail.com>
2019-08-05 22:25:21 +0100

    Update the sitemap

--- apps/diet_web/lib/diet_web/controllers/sitemap_controller.ex
+++ apps/diet_web/lib/diet_web/controllers/sitemap_controller.ex
@@ -2,10 +2,14 @@ defmodule DietWeb.SitemapController do
   use DietWeb, :controller
 
   def sitemap(conn, params) do
-    xml = DietWeb.Sitemaps.generate() |>  Enum.join()
+    xml =
+      DietWeb.Sitemaps.generate()
+      |> Enum.join()
+      |> String.replace(~r{<sitemapindex[^>]+>\n}, "")
+      |> String.replace(~r{</sitemapindex>\n}, "")
 
     conn
-    |> put_resp_content_type("text/xml")
+    |> put_resp_content_type("application/xml")
     |> send_resp(200, xml)
   end
 end
--- apps/diet_web/lib/diet_web/sitemaps/sitemaps.ex
+++ apps/diet_web/lib/diet_web/sitemaps/sitemaps.ex
@@ -10,17 +10,26 @@ defmodule DietWeb.Sitemaps do
 
   def generate do
     create adapter: @adapter, host: @host, compresion: false do
-      add Routes.page_path(Endpoint, :index), priority: 0.9, changefreq: "monthly"
+      add(Routes.page_path(Endpoint, :index), priority: 0.9, changefreq: "monthly")
 
       published_videos = Multimedia.list_published_videos(limit: 10_000)
       newest_video = Enum.max_by(published_videos, & &1.published_at)
-      add Routes.page_path(Endpoint, :newsfeed), priority: 0.9, changefreq: "always", lastmod: newest_video.published_at
 
-      add Routes.user_path(Endpoint, :new), priority: 0.7, changefreq: "monthly"
-      add Routes.session_path(Endpoint, :new), priority: 0.7, changefreq: "monthly"
+      add(Routes.page_path(Endpoint, :newsfeed),
+        priority: 0.9,
+        changefreq: "always",
+        lastmod: newest_video.published_at
+      )
+
+      add(Routes.user_path(Endpoint, :new), priority: 0.7, changefreq: "monthly")
+      add(Routes.session_path(Endpoint, :new), priority: 0.7, changefreq: "monthly")
 
       for video <- published_videos do
-        add Routes.watch_path(Endpoint, :show, video), priority: 0.5, changefreq: "hourly", lastmod: video.published_at
+        add(Routes.watch_path(Endpoint, :show, video),
+          priority: 0.5,
+          changefreq: "hourly",
+          lastmod: video.published_at
+        )
       end
     end
   end
