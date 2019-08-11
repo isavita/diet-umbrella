d1b5779e9dbc4254c6772e1ce57dc5485af78f52
Aleksandar Dimov <isavitaisa@gmail.com>
2019-08-11 22:25:54 +0100

    Remove default bootstrap and add bootstrap 4

--- apps/diet_web/assets/webpack.config.js
+++ apps/diet_web/assets/webpack.config.js
@@ -35,6 +35,10 @@ module.exports = (env, options) => ({
         }
       },
      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+     {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
