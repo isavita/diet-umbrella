033e63834c7cbad7f31c48afece497875c789b51
Aleksandar Dimov <isavitaisa@gmail.com>
2019-08-22 23:44:13 +0100

    Fix wrong import

--- apps/diet_web/assets/entrypoints/player.js
+++ apps/diet_web/assets/entrypoints/player.js
@@ -1,5 +1,4 @@
 import Player from "../js/player";
-import css from "../css/player.scss";
 
 const video = document.getElementById("video");
 Player.init(video.id, video.getAttribute("data-player-id"), () => {
