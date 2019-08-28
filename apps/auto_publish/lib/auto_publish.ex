415bf48fe03077056aa0eca6de73ea41d75d39df
Aleksandar Dimov <isavitaisa@gmail.com>
2019-08-28 21:51:07 +0100

    Remove redundant tests

--- apps/auto_publish/lib/auto_publish.ex
+++ apps/auto_publish/lib/auto_publish.ex
@@ -1,18 +1,2 @@
 defmodule AutoPublish do
-  @moduledoc """
-  Documentation for AutoPublish.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AutoPublish.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/auto_publish/test/auto_publish_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AutoPublishTest do
-  use ExUnit.Case
-  doctest AutoPublish
-
-  test "greets the world" do
-    assert AutoPublish.hello() == :world
-  end
-end
